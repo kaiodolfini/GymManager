7fa19c6a47226f14e6d80ab02a55aed9806ec058
Kaio Dolfini <kaio.dolfini@gmail.com>
2020-07-20 14:58:30 -0300

    Created at

--- instructors.js
+++ instructors.js
@@ -1,21 +1,25 @@
 const fs = require('fs')
+const data = require("./data.json")
 
-exports.post = function(req, res) {
+exports.post = function (req, res) {
 
     const keys = Object.keys(req.body)
 
-    for (key of keys){
-        if (req.body[key] == ""){
+    for (key of keys) {
+        if (req.body[key] == "") {
             return res.send('Please, fill all fields!')
         }
     }
 
-        fs.writeFile("data.json", JSON.stringify(req.body), function(err){
-            if(err) return res.send("Write file error!")
+    req.body.created_at = Date.now()
+    req.body.birth = Date.parse(req.body.birth)
+    
+    data.instructors.push(req.body)
 
-            return res.redirect("/instructors")
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function (err) {
+        if (err) return res.send("Write file error!")
+
+        return res.redirect("/instructors")
 
     })
-    //return res.send(req.body)
-
 } 
