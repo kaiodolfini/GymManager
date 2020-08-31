20ca3e7bff3c0253c449ba85ed2a4e21f8d4e0dc
Kaio Dolfini <kaio.dolfini@gmail.com>
2020-08-31 16:00:40 -0300

    Continuando as Aulas da Academia

--- instructors.js
+++ instructors.js
@@ -1,6 +1,26 @@
 const fs = require('fs')
 const data = require("./data.json")
 
+exports.show = function (req, res) {
+    //req.params
+    const { id } = req.params
+
+    const foundInstructor = data.instructors.find(function (instructor) {
+        return instructor.id == id
+    })
+
+    if (!foundInstructor) return res.send("Instructor not found !")
+
+    const instructor = {
+        ...foundInstructor,
+        age:"",
+        services: foundInstructor.services.split(","),
+        created_at:""
+    }
+
+    return res.render("instructors/show", {instructor})
+}
+
 exports.post = function (req, res) {
 
     const keys = Object.keys(req.body)
@@ -11,12 +31,12 @@ exports.post = function (req, res) {
         }
     }
 
-    let {avatar_url, birth, name, services, gender} = req.body
+    let { avatar_url, birth, name, services, gender } = req.body
 
     birth = Date.parse(birth)
     const created_at = Date.now()
     const id = Number(data.instructors.length + 1)
-    
+
     data.instructors.push({
         id,
         avatar_url,
